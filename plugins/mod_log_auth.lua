47678b7631d60ba6c456c41dca8eb506b3549eb4
Victor Seva <vseva@sipwise.com>
2014-05-21 12:47:08 +0200

    MT#6285 add mod_log_auth plugin

--- plugins/mod_log_auth.lua
+++ plugins/mod_log_auth.lua
@@ -1,3 +1,8 @@
+-- Copyright (C) 2013 Kim Alvefur <zash@zash.se>
+--
+-- This project is MIT/X11 licensed. Please see the
+-- COPYING file in the source package for more information.
+--
 
 module:hook("authentication-failure", function (event)
 	module:log("info", "Failed authentication attempt (%s) from IP: %s", event.condition or "unknown-condition", event.session.ip or "?");
