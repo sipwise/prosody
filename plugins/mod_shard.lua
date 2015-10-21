a4bfb6fbcd08831d94af2d3af848ea6cd46d5010
Victor Seva <vseva@sipwise.com>
2015-10-21 13:27:55 +0200

    MT#6407 cluster using mod_shard* as base
    https://github.com/robn/prosody/commit/5d5ee8db0d6b31056285a838c3e86f9da0088ed2
    https://groups.google.com/d/topic/prosody-dev/X99ktLm0JwU/discussion
    
    Change-Id: I1dbbe0075e3ca2ba28998a2d2f3396ab4293dbfb

---
+++ plugins/mod_shard.lua
@@ -0,0 +1,68 @@
+-- Prosody IM
+-- Copyright (C) 2015 Robert Norris <robn@robn.io>
+-- Copyright (C) 2015 Sipwise GmbH <development@sipwise.com>
+--
+-- This project is MIT/X11 licensed. Please see the
+-- COPYING file in the source package for more information.
+--
+module:depends("sipwise_redis_sessions");
+local redis_sessions = module:shared("/*/sipwise_redis_sessions/redis_sessions");
+local jid_split = require "util.jid".split;
+local fire_event = prosody.events.fire_event;
+local st = require "util.stanza";
+
+local shard_name = module:get_option("shard_name", nil);
+if not shard_name then
+    error("shard_name not configured", 0);
+end
+
+
+module:log("info", "%s added to shard %s", module.host, shard_name);
+
+
+local function handle_event (event)
+    local to = event.stanza.attr.to;
+    local node, host, resource = jid_split(to);
+
+    if not node or not host then
+        return nil
+    end
+    if host ~= module.host then
+        return nil
+    end
+
+    if resource and prosody.full_sessions[to] then
+        module:log("debug", "%s has a session here, nothing to do",
+            node..'@'..host);
+        return nil
+    end
+
+    module:log("debug", "looking up target shard for %s", to);
+
+    local rhosts = redis_sessions.get_hosts(to);
+    local res;
+    for shard,resources in pairs(rhosts) do
+        if shard and shard ~= shard_name then
+            for _,r in pairs(resources) do
+                local stanza_c = st.clone(event.stanza);
+                stanza_c.attr.to = node..'@'..host..'/'..r;
+                module:log("debug", "target shard for %s is %s",
+                    stanza_c.attr.to ,shard);
+                fire_event("shard/send", { shard = shard, stanza = stanza_c });
+                res = true;
+            end
+        end
+    end
+
+    return res;
+end
+
+module:hook("iq/bare", handle_event, 1000);
+module:hook("iq/full", handle_event, 1000);
+module:hook("iq/host", handle_event, 1000);
+module:hook("message/bare", handle_event, 1000);
+module:hook("message/full", handle_event, 1000);
+module:hook("message/host", handle_event, 1000);
+module:hook("presence/bare", handle_event, 1000);
+module:hook("presence/full", handle_event, 1000);
+module:hook("presence/host", handle_event, 1000);
