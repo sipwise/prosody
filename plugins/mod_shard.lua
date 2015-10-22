c55b78d727b01640b60784a5a19043fe5541d0ff
Victor Seva <vseva@sipwise.com>
2015-10-22 11:00:31 +0200

    MT#6407 support MUC on cluster
    Change-Id: I6d8cc8976ca32bb2af379e2bce9aae38551c7928

--- plugins/mod_shard.lua
+++ plugins/mod_shard.lua
@@ -7,18 +7,38 @@
 --
 module:depends("sipwise_redis_sessions");
 local redis_sessions = module:shared("/*/sipwise_redis_sessions/redis_sessions");
+local redis_mucs;
 local jid_split = require "util.jid".split;
 local fire_event = prosody.events.fire_event;
 local st = require "util.stanza";
-
+local uts = require "util.table".string;
 local shard_name = module:get_option("shard_name", nil);
 if not shard_name then
     error("shard_name not configured", 0);
 end
 
-
 module:log("info", "%s added to shard %s", module.host, shard_name);
 
+local function handle_room_event(event)
+    local to = event.stanza.attr.to;
+
+    module:log("debug", "looking up target room shard for %s", to);
+    local rhost = redis_sessions.get_room_host(to);
+
+    if not rhost then
+        module:log("debug", "room not found. Nothing to do");
+        return nil;
+    end
+
+    if rhost == shard_name then
+        module:log("debug", "room is hosted here. Nothing to do");
+        return nil
+    end
+
+    module:log("debug", "target shard for %s is %s", to, rhost);
+    fire_event("shard/send", { shard = rhost, stanza = event.stanza });
+    return true;
+end
 
 local function handle_event (event)
     local to = event.stanza.attr.to;
@@ -30,6 +50,14 @@ local function handle_event (event)
     end
     if host ~= module.host then
         return nil
+    end
+    if uts.starts(host, 'conference.') then
+        if redis_mucs then
+            module:log("debug", "MUC %s detected", host);
+            return handle_room_event(event);
+        else
+            module:log("debug", "redis_mucs nill");
+        end
     end
 
     if resource and prosody.full_sessions[to] then
@@ -62,12 +90,22 @@ local function handle_event (event)
     return stop_process_local;
 end
 
-module:hook("iq/bare", handle_event, 1000);
-module:hook("iq/full", handle_event, 1000);
-module:hook("iq/host", handle_event, 1000);
-module:hook("message/bare", handle_event, 1000);
-module:hook("message/full", handle_event, 1000);
-module:hook("message/host", handle_event, 1000);
-module:hook("presence/bare", handle_event, 1000);
-module:hook("presence/full", handle_event, 1000);
-module:hook("presence/host", handle_event, 1000);
+function module.add_host(module)
+    local host = module:get_host();
+    if module:get_host_type() == "component" then
+        module:depends("sipwise_redis_mucs");
+        redis_mucs = module:shared("/"..host
+            .."/sipwise_redis_mucs/redis_mucs");
+        module:log("debug", "enable MUC for %s", host);
+    end
+    module:hook("iq/bare", handle_event, 1000);
+    module:hook("iq/full", handle_event, 1000);
+    module:hook("iq/host", handle_event, 1000);
+    module:hook("message/bare", handle_event, 1000);
+    module:hook("message/full", handle_event, 1000);
+    module:hook("message/host", handle_event, 1000);
+    module:hook("presence/bare", handle_event, 1000);
+    module:hook("presence/full", handle_event, 1000);
+    module:hook("presence/host", handle_event, 1000);
+    module:log("debug", "hooked at %s", host);
+end
