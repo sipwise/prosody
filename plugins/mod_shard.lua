babc44fb9b160230627be42bd39eca9bd8bd6401
Victor Seva <vseva@sipwise.com>
2015-10-22 11:00:31 +0200

    MT#6407 support MUC on cluster
    Change-Id: I6d8cc8976ca32bb2af379e2bce9aae38551c7928

--- plugins/mod_shard.lua
+++ plugins/mod_shard.lua
@@ -7,33 +7,85 @@
 --
 module:depends("sipwise_redis_sessions");
 local redis_sessions = module:shared("/*/sipwise_redis_sessions/redis_sessions");
+local redis_mucs;
 local jid_split = require "util.jid".split;
 local fire_event = prosody.events.fire_event;
 local st = require "util.stanza";
-
+local ut = require "util.table";
 local shard_name = module:get_option("shard_name", nil);
 if not shard_name then
     error("shard_name not configured", 0);
 end
 
-
 module:log("info", "%s added to shard %s", module.host, shard_name);
 
+local function build_query_result(host, rooms, stanza)
+    local xmlns = 'http://jabber.org/protocol/disco#items';
+    local s = stanza:query(xmlns);
+    for _,v in ipairs(rooms) do
+        s:tag("item", {jid=v.."@"..host}):up();
+    end
+    return stanza;
+end
+
+local function handle_room_event(event)
+    local to = event.stanza.attr.to;
+    local node, host, _ = jid_split(to);
+    local rhost;
+
+    if node then
+        module:log("debug", "looking up target room shard for %s", to);
+        rhost = redis_mucs.get_room_host(to);
+    else
+        local l = redis_mucs.get_rooms(host);
+        module:log("debug", "rooms: %s", ut.table.tostring(l));
+        local stanza = build_query_result(host, l, st.reply(event.stanza));
+        module:log("debug", "reply[%s]", tostring(stanza));
+        event.origin.send(stanza);
+        return true;
+    end
+
+    if not rhost then
+        module:log("debug", "room not found. Nothing to do");
+        return nil;
+    end
+
+    if rhost == shard_name then
+        module:log("debug", "room is hosted here. Nothing to do");
+        return nil
+    end
+
+    module:log("debug", "target shard for %s is %s", to, rhost);
+    fire_event("shard/send", { shard = rhost, stanza = event.stanza });
+    return true;
+end
 
 local function handle_event (event)
     local to = event.stanza.attr.to;
     local node, host, resource = jid_split(to);
     local stop_process_local;
 
-    if not node or not host then
+    if not host then
+        module:log("debug", "no host. Nothing to do here");
         return nil
     end
-    if host ~= module.host then
-        return nil
+
+    if ut.string.starts(host, 'conference.') then
+        if redis_mucs then
+            module:log("debug", "MUC %s detected", host);
+            return handle_room_event(event);
+        else
+            module:log("debug", "redis_mucs nill");
+        end
     end
 
     if resource and prosody.full_sessions[to] then
         module:log("debug", "%s has a session here, nothing to do", to);
+        return nil
+    end
+
+    if not node then
+        module:log("debug", "no node. Nothing to do here");
         return nil
     end
 
@@ -62,6 +114,12 @@ local function handle_event (event)
     return stop_process_local;
 end
 
+local host = module:get_host();
+if module:get_host_type() == "component" then
+    module:depends("sipwise_redis_mucs");
+    redis_mucs = module:shared("/*/sipwise_redis_mucs/redis_mucs");
+    module:log("debug", "enable MUC for %s", host);
+end
 module:hook("iq/bare", handle_event, 1000);
 module:hook("iq/full", handle_event, 1000);
 module:hook("iq/host", handle_event, 1000);
@@ -71,3 +129,4 @@ module:hook("message/host", handle_event, 1000);
 module:hook("presence/bare", handle_event, 1000);
 module:hook("presence/full", handle_event, 1000);
 module:hook("presence/host", handle_event, 1000);
+module:log("debug", "hooked at %s", host);
